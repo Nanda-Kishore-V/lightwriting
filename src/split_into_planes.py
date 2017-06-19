1915b8bfcd12126f3518eef298ad374818046fcc
Nanda Kishore Vasudevan <nandakishore12111996@gmail.com>
2017-06-19 15:54:51 -0700

    Change location of workspace in code

--- src/split_into_planes.py
+++ src/split_into_planes.py
@@ -11,7 +11,7 @@ HEIGHT_OFFSET = 1
 X_OFFSET = 0.1
 FACTOR = 20
 EPSILON = 0.2 * FACTOR
-ROS_WS = "/home/nanda/Documents/Intern/crazyflie/crazyswarm/ros_ws/src/crazyswarm/"
+ROS_WS = "/home/nanda/Documents/Intern/crazyswarm/ros_ws/src/crazyswarm/"
 
 def second_largest(numbers):
     first, second = None, None
